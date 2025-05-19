0db974e47bb3716ae0b805c3800aac7b7d980b7b
mehdiiotmar <148407538+mehdiiotmar@users.noreply.github.com>
2025-05-19 01:13:14 +0100

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,10 +1,13 @@
+import React from "react";
+
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am eLMehdi -mksmart</h1>
+    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am eLMehdi - MKSMART</h1>
     <p>
-      You can use me to create your own chat bot app. 
-      So feel free to ask me about home automation! Then, create
+      You can use me to create your own chatbot app. 
+      Feel free to ask me about home automation! Then, create
       your own bot.
+    </p>
     <p className="mt-2">
       <a
         href="https://www.mksmart.ma/shop"
